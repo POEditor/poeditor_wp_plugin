b969717b993dc454e67e6191eb96a489eb93898a
Paula <paula@datasolutions.ro>
2023-05-11 10:44:12 +0300

    icon16 class removed

--- views/scan.php
+++ views/scan.php
@@ -4,12 +4,9 @@
         echo '<img src="' . plugins_url( '_resources/img/logo.png' , __FILE__ ) . '" alt="POEditor" > ';
         ?>
     </h2>
-    <div class="tool-box">
-        <div class="icon16">
-            <?php
-            echo '<img src="' . plugins_url( '_resources/img/preloader.gif' , __FILE__ ) . '" alt="Loading..." > ';
-            ?>
-        </div>
-        <p><?php esc_html_e('Scanning for language files. Please wait', 'poeditor'); ?>&hellip;</p>
+    <div class="tool-box" style="display: flex">
+        <?php echo '<img src="' . plugins_url( '_resources/img/spinner.svg' , __FILE__ ) . '" alt="Loading..." > '; ?>
+
+        <p style="margin-left: 5px;"><?php esc_html_e('Scanning for language files. Please wait', 'poeditor'); ?>&hellip;</p>
     </div>
 </div>
