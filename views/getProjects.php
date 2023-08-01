2e85852ff1f185ec415640752dd66cb274ff78d4
Paula <paula@datasolutions.ro>
2023-08-01 17:15:46 +0300

    functionality fixes -> sync btn, create project, upload done

--- views/getProjects.php
+++ views/getProjects.php
@@ -4,12 +4,9 @@
         echo '<img src="' . plugins_url( '_resources/img/logo_dark.svg' , __FILE__ ) . '" alt="POEditor" > ';
         ?>
     </h2>
-    <div class="tool-box">
-        <div class="icon16">
-            <?php
-            echo '<img src="' . plugins_url( '_resources/img/preloader.gif' , __FILE__ ) . '" alt="'.__('Loading', 'poeditor').'&hellip;" > ';
-            ?>
-        </div>
-        <p><?php esc_html_e('Retrieving online projects from POEditor.com. Please wait', 'poeditor'); ?>&hellip;</p>
+    <div class="tool-box" style="display: flex">
+        <?php echo '<img src="' . plugins_url( '_resources/img/spinner.svg' , __FILE__ ) . '" alt="Loading..." > '; ?>
+
+        <p style="margin-left: 5px;"><?php esc_html_e('Retrieving online projects from POEditor.com. Please wait', 'poeditor'); ?>&hellip;</p>
     </div>
 </div>
